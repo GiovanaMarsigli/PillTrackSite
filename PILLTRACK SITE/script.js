40254fb4e9c4e2a2cfbea84d58de749755e8c616
gibsgiba <marsigligiovana@gmail.com>
2024-11-06 14:05:23 -0300

    Atualizações

--- PILLTRACK SITE/script.js
+++ PILLTRACK SITE/script.js
@@ -1,25 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('ul li a');
-
-    function updateActiveLink() {
-        let current = '';
-        sections.forEach(section => {
-            const sectionTop = section.offsetTop;
-            const sectionHeight = section.clientHeight;
-            if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
-                current = section.getAttribute('id');
-            }
-        });
-
-        navLinks.forEach(link => {
-            link.classList.remove('active');
-            if (link.getAttribute('href') === `#${current}`) {
-                link.classList.add('active');
-            }
-        });
-    }
-
     document.addEventListener('scroll', updateActiveLink);
 
     updateActiveLink();
